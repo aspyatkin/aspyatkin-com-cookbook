886a65dac46217e193befbb65acbbfd3e6224e4b
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-01-09 16:31:31 +0400

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'aspyatkin-com'
 description 'Installs and configures aspyatkin.com'
-version '1.2.0'
+version '1.3.0'
 
 recipe 'aspyatkin-com', 'Installs and configures aspyatkin.com'
 depends 'latest-git', '~> 1.1.2'
