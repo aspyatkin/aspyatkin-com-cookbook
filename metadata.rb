b95268f34c2d5450db9fc5b82294a8c34266f1a7
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-01-17 19:54:29 +0400

    Simplify things by using modern_nginx cookbook's new features

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name 'aspyatkin-com'
 description 'Installs and configures aspyatkin.com'
-version '1.3.1'
+version '1.3.2'
 
 recipe 'aspyatkin-com', 'Installs and configures aspyatkin.com'
 depends 'latest-git', '~> 1.1.3'
 depends 'latest-nodejs', '~> 1.1.1'
-depends 'modern_nginx', '~> 1.1.0'
+depends 'modern_nginx', '~> 1.2.0'
 depends 'rbenv', '~> 1.7.1'
 depends 'ssh_known_hosts', '~> 2.0.0'
 depends 'ssh_user', '~> 0.1.1'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,18 +1,17 @@
-include_recipe 'latest-git'
-include_recipe 'latest-nodejs'
-include_recipe 'modern_nginx'
+include_recipe 'latest-git::default'
+include_recipe 'latest-nodejs::default'
+include_recipe 'modern_nginx::default'
+include_recipe 'modern_nginx::cert'
 include_recipe 'rbenv::default'
 include_recipe 'rbenv::ruby_build'
 
 id = 'aspyatkin-com'
 
-data_bag = data_bag_item(id, node.chef_environment).to_hash
-
 if node.chef_environment.start_with? 'development'
   node.default[id][:repository] = 'git@github.com:aspyatkin/aspyatkin.com.git'
   ssh_known_hosts_entry 'github.com'
 
-  data_bag.fetch('ssh', {}).each do |key_type, key_contents|
+  data_bag_item(id, node.chef_environment).to_hash.fetch('ssh', {}).each do |key_type, key_contents|
     ssh_user_private_key key_type do
       key key_contents
       user node[id][:user]
@@ -40,7 +39,7 @@ git base_dir do
 end
 
 if node.chef_environment.start_with? 'development'
-  data_bag.fetch('git_config', {}).each do |key, value|
+  data_bag_item(id, node.chef_environment).to_hash.fetch('git_config', {}).each do |key, value|
     git_config key do
       value value
       scope 'local'
@@ -54,16 +53,6 @@ end
 logs_dir = ::File.join base_dir, 'logs'
 
 directory logs_dir do
-  owner node[id][:user]
-  group node[id][:group]
-  mode 0755
-  recursive true
-  action :create
-end
-
-letsencrypt_dir = ::File.join base_dir, 'letsencrypt'
-
-directory letsencrypt_dir do
   owner node[id][:user]
   group node[id][:group]
   mode 0755
@@ -99,104 +88,31 @@ rbenv_execute 'Build website' do
   environment 'JEKYLL_ENV' => node.chef_environment
 end
 
-data_bag.fetch('letsencrypt', {}).each do |fqdn, entries|
-  letsencrypt_fqdn_dir = ::File.join letsencrypt_dir, fqdn
-
-  directory letsencrypt_fqdn_dir do
-    owner node[id][:user]
-    group node[id][:group]
-    mode 0755
-    recursive true
-    action :create
-  end
-
-  entries.each do |key, value|
-    path = ::File.join letsencrypt_fqdn_dir, key
-    file path do
-      owner node[id][:user]
-      group node[id][:group]
-      mode 0644
-      content value
-      action :create
-    end
-  end
-end
-
-cert_dir = ::File.join node[:nginx][:dir], 'cert'
-
-directory cert_dir do
-  owner 'root'
-  group node['root_group']
-  mode 0700
-  action :create
-end
-
-cert_entry = data_bag.fetch 'ssl', nil
-
-cert_name = cert_entry['domains'][0]
-ssl_certificate_path = ::File.join cert_dir, "#{cert_name}.chained.crt"
-
-file ssl_certificate_path do
-  owner 'root'
-  group node['root_group']
-  mode 0600
-  content cert_entry['chain']
-  action :create
-end
-
-ssl_certificate_key_path = ::File.join cert_dir, "#{cert_name}.key"
-
-file ssl_certificate_key_path do
-  owner 'root'
-  group node['root_group']
-  mode 0600
-  content cert_entry['private_key']
-  action :create
-end
-
-scts_dir = ::File.join node[:nginx][:dir], 'scts', cert_name
-
-directory scts_dir do
-  owner 'root'
-  group node['root_group']
-  mode 0700
-  recursive true
-  action :create
-end
-
-require 'base64'
-
-data_bag.fetch('scts', {}).each do |name, data|
-  path = ::File.join scts_dir, "#{name}.sct"
-  file path do
-    owner 'root'
-    group node['root_group']
-    mode 0644
-    content Base64.decode64 data
-    action :create
-  end
-end
-
 nginx_conf = ::File.join node[:nginx][:dir], 'sites-available', "#{node[id][:fqdn]}.conf"
 
 template nginx_conf do
+  Chef::Resource::Template.send(:include, ::ModernNginx::Helper)
   source 'nginx.conf.erb'
   mode 0644
   notifies :reload, 'service[nginx]', :delayed
   variables(
     fqdn: node[id][:fqdn],
-    letsencrypt_root: letsencrypt_dir,
-    ssl_certificate: ssl_certificate_path,
-    ssl_certificate_key: ssl_certificate_key_path,
+    acme_challenge: node.chef_environment.start_with?('production'),
+    acme_challenge_directories: {
+      "#{node[id][:fqdn]}" => get_acme_challenge_directory(node[id][:fqdn]),
+      "www.#{node[id][:fqdn]}" => get_acme_challenge_directory("www.#{node[id][:fqdn]}")
+    },
+    ssl_certificate: get_ssl_certificate_path(node[id][:fqdn]),
+    ssl_certificate_key: get_ssl_certificate_private_key_path(node[id][:fqdn]),
     hsts_max_age: node[id][:hsts_max_age],
     access_log: ::File.join(logs_dir, 'nginx_access.log'),
     error_log: ::File.join(logs_dir, 'nginx_error.log'),
     doc_root: ::File.join(base_dir, '_site'),
     oscp_stapling: node.chef_environment.start_with?('production'),
     scts: node.chef_environment.start_with?('production'),
-    scts_dir: scts_dir,
+    scts_dir: get_scts_directory(node[id][:fqdn]),
     hpkp: node.chef_environment.start_with?('production'),
-    hpkp_pins: data_bag.fetch('hpkp', []),
+    hpkp_pins: get_hpkp_pins(node[id][:fqdn]),
     hpkp_max_age: node[id][:hpkp_max_age]
   )
   action :create
