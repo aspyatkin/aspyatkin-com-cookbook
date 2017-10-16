4e4bd7fc6cbae77d00d476751006953ff556e45b
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-10-16 20:48:30 +0400

    Update nginx config

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'aspyatkin-com'
 description 'Installs and configures aspyatkin.com'
-version '1.7.0'
+version '1.7.1'
 
 recipe 'aspyatkin-com', 'Installs and configures aspyatkin.com'
 depends 'git', '~> 6.1.0'
@@ -10,3 +10,4 @@ depends 'rbenv', '~> 1.7.1'
 depends 'ssh_known_hosts', '~> 4.0.0'
 depends 'ssh-private-keys', '~> 2.0.0'
 depends 'tls', '~> 3.0.0'
+
