df7921b6586252059ac64817ec15433c8cf77b2d
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-11-04 17:06:07 +0400

    Update dependencies, make use of instance cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
 name 'aspyatkin-com'
 description 'Installs and configures aspyatkin.com'
-version '1.7.1'
+version '1.8.0'
 
 recipe 'aspyatkin-com', 'Installs and configures aspyatkin.com'
-depends 'git', '~> 6.1.0'
+depends 'git', '~> 8.0.0'
 depends 'latest-nodejs', '~> 1.4.0'
-depends 'chef_nginx', '~> 6.1.1'
+depends 'chef_nginx', '~> 6.2.0'
 depends 'rbenv', '~> 1.7.1'
 depends 'ssh_known_hosts', '~> 4.0.0'
 depends 'ssh-private-keys', '~> 2.0.0'
 depends 'tls', '~> 3.0.0'
-
+depends 'instance', '~> 1.0.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
 include_recipe 'latest-nodejs::default'
 
-h = ::ChefCookbook::AspyatkinCom::Helper.new(node)
+h = ::ChefCookbook::Instance::Helper.new(node)
 
 node.default['rbenv']['group_users'] = [
   h.root_user,
@@ -20,7 +20,8 @@ if node.chef_environment.start_with? 'development'
   repository_url = "git@github.com:#{node[id]['github_repository']}.git"
 end
 
-base_dir = ::File.join('/var/www', h.fqdn)
+fqdn = node[id]['fqdn'].nil? ? node['automatic']['fqdn'] : node[id]['fqdn']
+base_dir = ::File.join('/var/www', fqdn)
 
 directory base_dir do
   owner h.instance_user
@@ -85,29 +86,29 @@ rbenv_execute 'Build website' do
   environment 'JEKYLL_ENV' => node.chef_environment
 end
 
-tls_rsa_certificate h.fqdn do
+tls_rsa_certificate fqdn do
   action :deploy
 end
 
-tls_rsa_item = ::ChefCookbook::TLS.new(node).rsa_certificate_entry(h.fqdn)
+tls_rsa_item = ::ChefCookbook::TLS.new(node).rsa_certificate_entry(fqdn)
 
-tls_ec_certificate h.fqdn do
+tls_ec_certificate fqdn do
   action :deploy
 end
 
-tls_ec_item = ::ChefCookbook::TLS.new(node).ec_certificate_entry(h.fqdn)
+tls_ec_item = ::ChefCookbook::TLS.new(node).ec_certificate_entry(fqdn)
 
-nginx_site h.fqdn do
+nginx_site fqdn do
   template 'nginx.conf.erb'
   variables(
-    fqdn: h.fqdn,
+    fqdn: fqdn,
     ssl_rsa_certificate: tls_rsa_item.certificate_path,
     ssl_rsa_certificate_key: tls_rsa_item.certificate_private_key_path,
     ssl_ec_certificate: tls_ec_item.certificate_path,
     ssl_ec_certificate_key: tls_ec_item.certificate_private_key_path,
     hsts_max_age: node[id]['hsts_max_age'],
-    access_log: ::File.join(node['nginx']['log_dir'], "#{h.fqdn}_access.log"),
-    error_log: ::File.join(node['nginx']['log_dir'], "#{h.fqdn}_error.log"),
+    access_log: ::File.join(node['nginx']['log_dir'], "#{fqdn}_access.log"),
+    error_log: ::File.join(node['nginx']['log_dir'], "#{fqdn}_error.log"),
     doc_root: ::File.join(base_dir, '_site'),
     oscp_stapling: node.chef_environment.start_with?('production'),
     scts: node.chef_environment.start_with?('production'),
