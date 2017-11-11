9180b4d735793758e6093800bcfa13e33a0b2abd
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-11-11 16:33:19 +0400

    Make use of updated `instance` and `nginx` cookbooks as well as `secret` cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,10 @@
 include_recipe 'latest-nodejs::default'
 
-h = ::ChefCookbook::Instance::Helper.new(node)
+instance = ::ChefCookbook::Instance::Helper.new(node)
 
 node.default['rbenv']['group_users'] = [
-  h.root_user,
-  h.instance_user
+  instance.root,
+  instance.user
 ]
 
 include_recipe 'rbenv::default'
@@ -15,7 +15,7 @@ id = 'aspyatkin-com'
 repository_url = "https://github.com/#{node[id]['github_repository']}"
 
 if node.chef_environment.start_with? 'development'
-  ssh_private_key h.instance_user
+  ssh_private_key instance.user
   ssh_known_hosts_entry 'github.com'
   repository_url = "git@github.com:#{node[id]['github_repository']}.git"
 end
@@ -24,8 +24,8 @@ fqdn = node[id]['fqdn'].nil? ? node['automatic']['fqdn'] : node[id]['fqdn']
 base_dir = ::File.join('/var/www', fqdn)
 
 directory base_dir do
-  owner h.instance_user
-  group h.instance_group
+  owner instance.user
+  group instance.group
   mode 0755
   recursive true
   action :create
@@ -35,23 +35,20 @@ git base_dir do
   repository repository_url
   revision node[id]['revision']
   enable_checkout false
-  user h.instance_user
-  group h.instance_group
+  user instance.user
+  group instance.group
   action :sync
 end
 
 if node.chef_environment.start_with?('development')
-  git_config = data_bag_item('git', node.chef_environment).to_hash.fetch(
-    'config',
-    {}
-  )
+  git_config = secret.get('git:config', prefix_fqdn: false, default: {})
 
   git_config.each do |key, value|
     git_config key do
       value value
       scope 'local'
       path base_dir
-      user h.instance_user
+      user instance.user
       action :set
     end
   end
@@ -73,16 +70,16 @@ rbenv_execute "Install bundle at #{base_dir}" do
   command 'bundle'
   ruby_version node[id]['ruby_version']
   cwd base_dir
-  user h.instance_user
-  group h.instance_group
+  user instance.user
+  group instance.group
 end
 
 rbenv_execute 'Build website' do
   command 'jekyll build'
   ruby_version node[id]['ruby_version']
   cwd base_dir
-  user h.instance_user
-  group h.instance_group
+  user instance.user
+  group instance.group
   environment 'JEKYLL_ENV' => node.chef_environment
 end
 
